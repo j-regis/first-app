28c73e0d98fedce27d9f3d689c747f2ec036b45c
James Regis <regis@lix.polytechnique.fr>
2017-09-21 16:45:57 -0400

    New version of application

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='MyFirstApp',
+      version='1.0',
+      description='OpenShift App',
+      author='James Regis',
+      author_email='james@freidojo.net',
+      url='http://www.python.org/sigs/distutils-sig/',
+      install_requires=['flask_restful'],
+     )
