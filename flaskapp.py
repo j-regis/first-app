fcf71a955be37662b2f461aa8722451d3b967158
James Regis <regis@lix.polytechnique.fr>
2017-09-23 09:02:37 -0400

    One modification

--- flaskapp.py
+++ flaskapp.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World from <b>openshift</b>'
+    return 'Hello World from <b>openshift first python app</b>'
 
 if __name__ == '__main__':
     app.run()
